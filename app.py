fee90adba982dbeed57c8c55f6121e064eac4f23
RichGHall <hallrich1@gmail.com>
2025-03-05 20:39:18 +0000

    20250405 24

--- app.py
+++ app.py
@@ -14,75 +14,68 @@ st.set_page_config(layout="wide")
 st.title('111 Mental Health (Option 2) Call Centre Discrete Event Simulation Model')
 st.write('This is some blurb describing the model')
 
-tab0, tab1, tab2, tab3, tab4 = st.tabs(["About","1, Demand", "2, Staffing", "3, Other Variables","4, Run"])
+tab0, tab1 = st.tabs(["The Model","About"])
 
+with st.sidebar:
+    st.write("This is the sidebar")
+    st.markdown("[Update Demand](#1-Demand)")
+    st.markdown("[Update Demand](#2-Staff)")
+    st.markdown("[Update Demand](#3-CallLength)")
+    st.markdown("[Update Demand](#4-Other)")
+    st.markdown("[Update Demand](#5-Run)")
 
 with tab0:
-    st.header("Introduction")
-    st.write("This model is designed to simulate the operation of a mental health call centre.")
-    st.write("The model is based on the following assumptions:")
-      
+    st.header("The Model")
+    st.write("Use this section to .")
 
-with tab1: 
-    st.header("Demand Details")
-    st.write("Use the grid below to update the expected number of calls per hour")
+    st.markdown("### Demand {#1-Demand}")
+
+    st.write("Use the grids below to update the expected number of calls per hour.")
+    st.write("This should be new callers only, the model will calcuate whether they will call back or not")
+
         #setup columns
-    col1, col2, col3 = st.columns(3)
 
-    #column related to public caller variables
-    with col1:
-        st.subheader("Public Callers")
+    st.subheader("Public Line - Average Calls per hour")
+    st.write("Use the grid below to update the expected number of calls per hour for the Public Line")
 
-        public_df = pd.read_csv("https://raw.githubusercontent.com/RichGHall/FRS_Model/main/files/Public_Av_Calls.csv")
-        edited_public_df = st.data_editor(public_df, num_rows=24)
+    public_df = pd.read_csv("https://raw.githubusercontent.com/RichGHall/FRS_Model/main/files/Public_Av_Calls.csv")
+    edited_public_df = st.data_editor(public_df, num_rows=24)
               
-    with col2:
-        st.subheader("Professional Callers")
+    
+    st.subheader("Professional Line - Average Calls per hour")
+    st.write("Use the grid below to update the expected number of calls per hour for the Professional Line")
 
-        prof_df = pd.read_csv("https://raw.githubusercontent.com/RichGHall/FRS_Model/main/files/Prof_Av_Calls.csv")  
-        edited_prof_df = st.data_editor(prof_df, num_rows=24)
+    prof_df = pd.read_csv("https://raw.githubusercontent.com/RichGHall/FRS_Model/main/files/Prof_Av_Calls.csv")  
+    edited_prof_df = st.data_editor(prof_df, num_rows=24)
 
-with tab2:  
-    st.header("Staffing Details")
-    st.write("Use the grid below to update the expected staffing levels for each hour")
+    st.markdown("### Staffing Levels {#2-Staff}")
+
+    st.write("Use the grid below to update the number of staff on shift per hour")
+    st.subheader("Staffing Levels")
 
     staffinf_df = pd.read_csv("https://raw.githubusercontent.com/RichGHall/FRS_Model/main/files/Senior_Resources.csv")  
     df_staff_renamed  = staffinf_df.rename(columns={"t": "Hour", "res": "Staff"})
     df_staff_renamed["Hour"] =  df_staff_renamed["Hour"] / 60
     
     df_staff_edited = st.data_editor(df_staff_renamed, num_rows=24) 
-    
-
-with tab3:
-    ## description of the tab and how to use it
-    st.header("Other Variables")
-    st.write("Use this page to change other model variables, such as call length, work time, break time ect")
-
-    st.subheader("No. Runs")
-    st.write("How many times will you loop through the model ?")
-    no_runs = st.slider("Number of model runs",min_value=0,max_value=500,value=1)   
-
-    #setup columns
-    col1, col2, col3 = st.columns(3)
-
-    #column related to public caller variables
-    with col1:
-        st.subheader("Public Callers")
-        
 
 
-        #public talk time
-        st.text("Talk Time")
-        st.write("How long will calls last ?")
-        pub_call_len = st.slider("Public Call Length (minutes)",min_value=0,max_value=60,value=1)   
+    st.markdown("### Call Length {#3-CallLength}")
+    st.write("Use the sliders below to udate the legnth of calls and post-call admin")
 
-        st.text("Write Up Time")
-        st.write("How much time is spent on post-call admin ?")
-        pub_work_len = st.slider("Public Work Length (minutes)",min_value=0,max_value=60,value=1)
 
-        st.text("Call Back Probability - Handled Calls")
-        st.write("Chance of calling back following a successful call ?")
-        pub_cb_han = st.slider(
+    st.subheader("Public Callers")
+    st.text("Talk Time")
+    st.write("How long will calls last ?")
+    pub_call_len = st.slider("Public Call Length (minutes)",min_value=0,max_value=60,value=1)   
+
+    st.text("Write Up Time")
+    st.write("How much time is spent on post-call admin ?")
+    pub_work_len = st.slider("Public Work Length (minutes)",min_value=0,max_value=60,value=1)
+
+    st.text("Call Back Probability - Handled Calls")
+    st.write("Chance of calling back following a successful call ?")
+    pub_cb_han = st.slider(
                                 "Public Call Back Probability Handled",
                                 min_value=0.0,    # float
                                 max_value=1.0,    # float
@@ -90,45 +83,49 @@ with tab3:
                                 step=0.01         # float
                             )
  
-        st.text("Call Back Probability - Abandoned Calls")
-        st.write("Chance of calling back following an unsuccessful call ?")
-        pub_cb_abd = st.slider(
+    st.text("Call Back Probability - Abandoned Calls")
+    st.write("Chance of calling back following an unsuccessful call ?")
+    pub_cb_abd = st.slider(
                                 "Public Call Back Probability Abandoned",
                                 min_value=0.0,    # float
                                 max_value=1.0,    # float
                                 value=0.01,       # float
                                 step=0.01         # float
-                            )
-    
-    #left as a gap for formating purposes
-    with col2:
-        st.write(" ")
 
-    #column related to public caller variables
-    with col3:
- 
-        st.subheader("Professional Callers")
+
+    st.subheader("Professional Callers")
         
-        st.text("Talk Time")
-        st.write("How long will calls last ?")
-        pub_call_len = st.slider("Prof Call Length (minutes)",min_value=0,max_value=60,value=1)   
+    st.text("Talk Time")
+    st.write("How long will calls last ?")
+    pub_call_len = st.slider("Prof Call Length (minutes)",min_value=0,max_value=60,value=1)   
 
-        st.text("Write Up Time")
-        st.write("How much time is spent on post-call admin ?")
-        pub_work_len = st.slider("Prof Work Length (minutes)",min_value=0,max_value=60,value=1)
+    st.text("Write Up Time")
+    st.write("How much time is spent on post-call admin ?")
+    pub_work_len = st.slider("Prof Work Length (minutes)",min_value=0,max_value=60,value=1)
 
-        st.text("Call Back Probability - Handled Calls")
-        st.write("Chance of calling back following a successful call ?")
+    st.text("Call Back Probability - Handled Calls")
+    st.write("Chance of calling back following a successful call ?")
    #     pub_cb_han = st.slider("Prof Call Back Probability Handled",min_value=0,max_value=1,value=0.01,step=0.01)
  
-        st.text("Call Back Probability - Abandoned Calls")
-        st.write("Chance of calling back following an unsuccessful call ?")
+    st.text("Call Back Probability - Abandoned Calls")
+    st.write("Chance of calling back following an unsuccessful call ?")
    #     pub_cb_abd = st.slider("Prof Call Back Probability Abd",min_value=0,max_value=1,value=0.01,step=0.01)
 
+    st.markdown("### Other {#4-Other}")
+    
+
+    ## description of the tab and how to use it
+    st.subheader("Other Variables")
+    st.write("Use this page to change other model variables")
+
+    st.subheader("No. Runs")
+    st.write("How many times will you loop through the model ?")
+    no_runs = st.slider("Number of model runs",min_value=0,max_value=100,value=40)   
 
 
 
-with tab4:
+    st.markdown("### Run the model {#5-Run}")
+
     st.header("Run")
     st.write("Use the button below to run the model")
     run_model = st.button("Run Model")
